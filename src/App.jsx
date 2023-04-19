2b925ad0c9932f1ca92674655bb5b4c7d29650fa
Sathya pramod <sathyapramod12@gmail.com>
2023-04-19 15:13:20 +0000

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import React, { component } from 'react';
+import React, { Component } from 'react';
 
 const Welcome = (props) => {
   return <h1>Meet the <i>{props.name}</i></h1>
+}
+
+class Hello extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  render (){
+    return <h1>Meet the {this.props.name }</h1>;
+  }
 }
 
 function App() {
@@ -12,8 +21,8 @@ function App() {
         <hgroup>
           <img src="images/group.svg" alt="StarGazers Group" />
           <Welcome name="Stars"/>
-          <Welcome />
-          <Welcome />
+          <Hello name="Cartoons"/>
+          <Welcome name="Aliens"/>
           <p>Members of an <b>intergalactic alliance</b><br />
             paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
           <button className="outline" onClick={() => alert('Hi there')}>Click Me</button>
